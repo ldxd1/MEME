0ec25bdc229ab0e3f1d3310951182c6b14a55899
Victor Defoe <victor.88@foxmail.com>
2020-02-07 01:38:28 +0800

    unfinished saved

--- nCov_19.py
+++ nCov_19.py
@@ -1,10 +1,40 @@
 
 # This is a project to predict the tendancy of WuHan 19 CoVirus 
 
+import pandas as pd
+import os
+from os.path import join as opj
 
 
+def data_read():
+    
+    dp = './19_nCoV_data/data-sources/dxy/data/'
+    namelist = os.listdir(dp)
+    namelist = [name for name in namelist if name[-3:] == 'csv']
+    for n in namelist:
+        data_org = opj(dp, n)
 
-def data_read()
+        a = pd.read_csv(data_org, header=None)
+        a = a.iloc[3:-1].reset_index(drop=True)
+
+        pp = [[], [], []]
+        for each in a[0]:
+            pp[0].append(each.split('|')[0])
+            pp[1].append(int(each.split('|')[1]))
+            pp[2].append(int(each.split('|')[2]))
+        pp[0].append('China Total')
+        pp[1].append(sum(pp[1]))
+        pp[2].append(sum(pp[2]))
+        places = pd.Series(pp[0])
+        confs = pd.Series(pp[1])
+        deaths = pd.Series(pp[2])
+        new_df = pd.DataFrame(columns=['place', 'confirmed','deaths'])
+        new_df['place'] = places
+        new_df['confirmed'] = confs
+        new_df['deaths'] = deaths
+
+        print(n)
+        print(new_df)
 
 
 
@@ -16,3 +46,6 @@ class SEIR(object):
     """
     def __init__(self):
         pass
+
+if __name__ == '__main__':
+    data_read()
