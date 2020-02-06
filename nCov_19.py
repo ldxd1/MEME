e41057e3c889d21f906b826314038df31b783d93
Victor Defoe <victor.88@foxmail.com>
2020-02-07 00:27:11 +0800

    init_added

---
+++ nCov_19.py
@@ -0,0 +1,18 @@
+
+# This is a project to predict the tendancy of WuHan 19 CoVirus 
+
+
+
+
+def data_read()
+
+
+
+
+
+class SEIR(object):
+    """
+    build up an SEIR model for 
+    """
+    def __init__(self):
+        pass
---
+++ tools.py
@@ -0,0 +1,3 @@
+
+import geatpy as ea 
+
